89db48c2c05ba73c2355f62d6021b1aa0ae60e3d
Nouri-Muhammad <nouri.muhammad91@gmail.com>
2024-02-22 09:31:32 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI(
+    title="Hotel Room Reservation Fast API",
+    description="A Website For Managing Hotel Room Reservations.",
+    version="0.0.1",
+    contact={
+        "name": "Nouri Muhammad",
+        "email": "nouri.muhammad1991@gmail.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
+
+
+@app.get("/")
+async def root():
+    return {"msg": "Let's Explore Allah's Creation! We provide you with a place to stay."}
